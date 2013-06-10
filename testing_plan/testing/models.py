24198e936937963388c527a1c56793cf026fef14
Marcos Gabriel Alcazar <marcos.alcazar@gmail.com>
2013-06-10 17:08:29 -0300

    Extra information for requirements

--- testing_plan/testing/models.py
+++ testing_plan/testing/models.py
@@ -1,15 +1,32 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse_lazy
 
 
 class Requirement(models.Model):
-
-    name = models.CharField(verbose_name=_('Name'), max_length=255)
     
+    REQUIREMENT_TYPES = (
+        ('FC', _('Functional - Use case')),
+        ('FS', _('Functional - Security')),
+        ('US', _('Usability')),
+        ('RE', _('Reliability')),
+        ('PE', _('Performance')),
+        ('SU', _('Supportability')),
+        ('IM', _('(+) Implementation')),
+        ('IN', _('(+) Interfaces (Not GUI)')),
+        ('OP', _('(+) Operations')),
+        ('EM', _('(+) Packaging')),
+        ('LE', _('(+) Legals')),
+    )
+
+    type = models.CharField(verbose_name=_('Requirement Type'), 
+                            max_length=2,
+                            choices=REQUIREMENT_TYPES)
+    name = models.CharField(verbose_name=_('Name'), max_length=255)
+
     def __unicode__(self):
-        return self.name
+        return u"[%s] %s" % (self.get_type_display(), self.name)
     
     def get_absolute_url(self):
         return reverse_lazy('testing:requirement_update', 
