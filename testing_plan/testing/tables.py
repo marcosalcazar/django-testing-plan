400f3266540a2260f1825b8ed628dc8a78e4ef96
Marcos Gabriel Alcazar <marcos.alcazar@gmail.com>
2013-06-11 14:50:03 -0300

    Menu reordering. Missing translations

--- testing_plan/testing/tables.py
+++ testing_plan/testing/tables.py
@@ -6,7 +6,8 @@ from django.utils.translation import ugettext_lazy as _
 class RequirementTable(tables.Table):
     test_cases = tables.TemplateColumn("{{ record.test_cases.all|length }}",
                                        verbose_name=_("Number of Test Cases"))
-    actions = tables.TemplateColumn(template_name='__record_actions.html')
+    actions = tables.TemplateColumn(template_name='__record_actions.html',
+                                    verbose_name=_("Actions"))
     
     class Meta:
         model = Requirement
